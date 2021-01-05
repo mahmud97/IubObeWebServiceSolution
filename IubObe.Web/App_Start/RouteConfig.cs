a7217fc0fb7c0de77995b98ec3c431baddba013b
Mahmud <mahmudulislam97@gmail.com>
2021-01-05 15:47:48 +0600

    excel service added

--- IubObe.Web/App_Start/RouteConfig.cs
+++ IubObe.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace IubObe.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ReadExcelData", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ IubObe.Web/Controllers/ReadExcelDataController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IubObe.Web.Controllers
+{
+    public class ReadExcelDataController : Controller
+    {
+        // GET: ReadExcelData
+        public ActionResult Index()
+        {
+            string con =
+              @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\HP\Desktop\CSE303 OBE Mark Sheet.xlsx;" +
+              @"Extended Properties='Excel 8.0;HDR=Yes;'";
+            using (OleDbConnection connection = new OleDbConnection(con))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand("select * from [Verdict$]", connection);
+                using (OleDbDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        var row1Col0 = dr[0];
+                        Console.WriteLine(row1Col0);
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
